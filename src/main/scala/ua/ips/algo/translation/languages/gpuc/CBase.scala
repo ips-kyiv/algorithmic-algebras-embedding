813bcdf4d4cac9eddd86d6f5c6e34d50d5a9ee48
Ruslan Shevchenko <ruslan@shevchenko.kiev.ua>
2021-09-19 21:37:23 +0300

    minimizing

--- src/main/scala/ua/ips/algo/translation/languages/gpuc/CBase.scala
+++ src/main/scala/ua/ips/algo/translation/languages/gpuc/CBase.scala
@@ -1,24 +1,9 @@
 package ua.ips.algo.translation.languages.gpuc
 
 
-import ua.ips.algo._
-import ua.ips.algo.translation._
-import java.io._
-import java.nio.file._
-
 
 trait CBase  {
 
-
-  def outputFile(dataDir: Path, fname: String): PrintWriter = 
-    ???
-    
-  def generateName(fullName: Seq[String]): String =
-    ???
-
-  def genMainNode(irCtx: IRContext, name:String): TranslationUnit = 
-    ???
-  
 
 
   def toCastExpression(expr: Expression): CastExpression =
@@ -27,7 +12,5 @@ trait CBase  {
       case _ => WrappedExpression(expr)
 
 
-  //def generateConstantExpression(vtx: CBaseGenContext, sort: DataSort, value: String): Expression =
-  //  ???
 
 }
