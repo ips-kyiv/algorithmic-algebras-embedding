51873cc26990496d27cf938cb60e72f822fb8206
Ruslan Shevchenko <ruslan@shevchenko.kiev.ua>
2021-10-10 19:54:48 +0300

    attempt to setuo JDK17 on build

--- src/main/scala/ua/ips/algo/translation/languages/clike/Jep412CallMarshalling.scala
+++ src/main/scala/ua/ips/algo/translation/languages/clike/Jep412CallMarshalling.scala
@@ -1,10 +1,10 @@
 package ua.ips.algo.translation.languages.clike
 
-
+/*
 import jdk.incubator.foreign.*
 import jdk.incubator.foreign.CLinker.*
 import jdk.incubator.foreign.CLinker.VaList.Builder
-
+*/
 
 object Jep412CallMarshalling {
 
